950b56166cd7332cf298627e231e515a0b50db1b
dashoreAd <83025362+dashoreAd@users.noreply.github.com>
2024-01-24 05:49:25 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all Js code as newer version
+
+// alert( 3 + 3 )we are using nodejs, not browser
+
+let name = "Aditya"
+let age = 24;
+let isLoggedIn = false
+
+
+// number ki range => 2 to the power 52-53
+// bigInt = number se jyada range chaahiye ho to
+// string => ""
+// boolean => true/false
+// null => standAlone value hai
+// undefined => abhi value assigned ni hui hai
+// symbol => unique
+// object
+
+
+console.log(typeof "Aditya");
+console.log(typeof age);
+console.log(typeof undefined);
+console.log(typeof null);  //object
