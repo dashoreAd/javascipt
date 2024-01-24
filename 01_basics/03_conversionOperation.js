a13a92d8ae024eea821e09d0ecb424d1b6a16d09
dashoreAd <83025362+dashoreAd@users.noreply.github.com>
2024-01-24 15:07:33 +0000

    coversion_operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,10 @@
 let score = "33abc"
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueIntNumber = Number(score)
-console.log(typeof valueIntNumber);
-console.log(valueIntNumber);
-
+// console.log(typeof valueIntNumber);
+// console.log(valueIntNumber);
 
 // "33" => 33
 // "33abc" => NaN
@@ -14,12 +13,56 @@ console.log(valueIntNumber);
 
 let isLoggedIn= 1
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 // "" => false
 // "aditya" => true
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// **************************** Operations ***********************
+
+let value = 3 
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%3)
+
+let str1 = "hello"
+let str2 = " Aditya"
+let str3 = str1 + str2
+console.log(str3);
+
+
+console.log("1" + 2)  
+// 12
+console.log(1 + "2")
+// 12
+console.log("1" + 2 + 2)
+// 122
+console.log(1 + 2 + "2")
+// 32
+console.log( (3+4) * 5 % 3)
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3 
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+
