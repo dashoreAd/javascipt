7b77907ce9b075dee6503ffdb02d93658872839c
dashoreAd <83025362+dashoreAd@users.noreply.github.com>
2024-01-24 15:50:34 +0000

    datatypes

---
+++ datatypes-summary.js
@@ -0,0 +1,32 @@
+// Primitive
+//  7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId);
+// false
+
+const bigNumber = 123456789987653n
+// console.log(typeof bigNumber); -> bigInt
+
+// Reference (Non primitive)
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "nagraj", "doga"]
+let myObj = {
+    name: "Aditya",
+    age: 24
+}
+const myFunction = function(){
+    console.log("Hello World");
+}
+console.log(typeof heros);
+console.log(typeof myObj);
+console.log(typeof myFunction);
+console.log(typeof anotherId);
