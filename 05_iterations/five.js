0a461fd6704619e814ccd7f4a279ec5f52badeaa
dashoreAd <83025362+dashoreAd@users.noreply.github.com>
2024-01-29 07:13:20 +0000

    five

---
+++ 05_iterations/five.js
@@ -0,0 +1,34 @@
+const coding = ["js", "ruby", "java", "python", "cpp"]
+
+// coding.forEach( function (val){
+//     console.log(val);
+// } )
+// for each loop by arrow function
+// coding.forEach((item=>{
+//     console.log(item);
+// }))
+
+// coding.forEach((item, index, arr)=>{
+//     console.log(item, index, arr);
+// })
+// js 0 [ 'js', 'ruby', 'java', 'python', 'cpp' ]
+// ruby 1 [ 'js', 'ruby', 'java', 'python', 'cpp' ]
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+    {
+        languageName: "python",
+        languageFileName: "py"
+    },
+]
+myCoding.forEach( (item) => {
+    
+    console.log(item.languageFileName);
+} )
