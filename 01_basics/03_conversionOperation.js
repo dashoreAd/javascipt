74b5f0ac0805a491a670e8b6863369cb38e4cade
dashoreAd <83025362+dashoreAd@users.noreply.github.com>
2024-02-02 14:00:02 +0000

    conversion_Operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -39,21 +39,21 @@ let negValue = -value
 let str1 = "hello"
 let str2 = " Aditya"
 let str3 = str1 + str2
-console.log(str3);
+// console.log(str3);
 
 
-console.log("1" + 2)  
+// console.log("1" + 2)  
 // 12
-console.log(1 + "2")
+// console.log(1 + "2")
 // 12
-console.log("1" + 2 + 2)
+// console.log(1 + "2" + 2)
 // 122
-console.log(1 + 2 + "2")
+// console.log(1 + 2 + "2")
 // 32
-console.log( (3+4) * 5 % 3)
+// console.log( (3+4) * 5 % 3)
 
-console.log(+true);
-console.log(+"");
+// console.log(+true);
+// console.log(+"");
 
 let num1, num2, num3 
 num1 = num2 = num3 = 2+2
