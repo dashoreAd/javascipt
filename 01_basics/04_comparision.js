4452b6b5a0a7a23a15a6cf928f6a2563df017f7e
dashoreAd <83025362+dashoreAd@users.noreply.github.com>
2024-02-02 13:59:32 +0000

    comparision

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -5,17 +5,17 @@
 // console.log(2 != 1);
 
 
-console.log("2" > 1);
+// console.log("2" > 1);
 // automatic convert krdiya 2(string) ko no. mai
-console.log("02" > 1);
+// console.log("02" > 1);
 
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
 
-console.log(undefined == 0);
-console.log(undefined > 0);
-console.log(undefined < 0);
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
 
 //  === -> data type bhi chek krta h
-console.log("2" === 2);
+// console.log("2" === 2);
