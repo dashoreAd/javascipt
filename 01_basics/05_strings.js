32b7732a46bc63c8c061e6fd952f6245f8d2ba16
dashoreAd <83025362+dashoreAd@users.noreply.github.com>
2024-03-18 05:40:05 +0000

    string

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -8,31 +8,37 @@ const repoCount = 50
 
 
 const gameName = new String('Aditya-Dashore')
+// ye tarike se bhot saare prototype use kr skte hai.
 
 // console.log(gameName[0]);
 // console.log(gameName.__proto__);
-
-
+// {}
 // console.log(gameName.length);
 // console.log(gameName.toUpperCase());
 // console.log(gameName.charAt(2));
 // console.log(gameName.indexOf('t'));
 
 const newString = gameName.substring(0, 4)
+// zeroth position se 4-1 tk chalega 
 // console.log(newString);
 // Adit
 
-const anotherString = gameName.slice(-1, 4)
-console.log(anotherString);
+const anotherString = gameName.slice(1, 4)
+// first position se 4-1 tk chalega 
+// console.log(anotherString);
 
 const newStringOne = "   hitesh    "
-// console.log(newStringOne.length);
-// console.log(newStringOne.trim().length);
+//  console.log(newStringOne.length);
+//  total character dega 13
+//  console.log(newStringOne.trim().length);
+//  aage piche ke spaces ko cut krke dega 6
+
 
 const url = "https://hitesh.com/hitesh%20choudhary"
 
-console.log(url.replace('%20', '-'))
-
-console.log(url.includes('sundar'))
-
-console.log(gameName.split('-'));
+// console.log(url.replace('%20', '-'))
+//  https://hitesh.com/hitesh-choudhary
+// console.log(url.includes('sundar'))
+//  false
+// console.log(gameName.split('-'));
+//  [ 'Aditya', 'Dashore' ]
