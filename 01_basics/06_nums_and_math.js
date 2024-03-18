dcf6782588389df2d5749d14e987810fa4bea9c7
dashoreAd <83025362+dashoreAd@users.noreply.github.com>
2024-03-18 09:30:42 +0000

    Edited

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -2,19 +2,24 @@ const score = 400
 // console.log(score);
 const balance = new Number(1000)
 // console.log(balance);
-console.log(balance.toString().length);
-// 4
-console.log(balance.toFixed(2));
+// [Number: 1000] - specially no. likha type milega
+// console.log(balance.toString());
+
+// 1000 - isse toString se ye string mai hogya, ab bhot sari propety aai
+// console.log(balance.toString().length);
+// 4 ye length btara h 1000 ki
+
+// console.log(balance.toFixed(2));
 // tofixed se woh decimal ke baad do zero tk value dega like 
-// 100.00
+// 1000.00
 const otherNumber = 123.8966
 // console.log(otherNumber.toPrecision(4));
-// 124
+// 124 - ye string return krta h
 const hundreds = 1000000;
 // console.log(hundreds.toLocaleString());
-// 1,000,000 -> us standard ke hisaab se dega
+// 1,000,000 -> US standard ke hisaab se dega
 // console.log(hundreds.toLocaleString('en-IN'));
-// 10,00,000
+// 10,00,000 -> Indian standard ke hisaab se dega
 // Number.MAX_VALUE -> 1.79E+308.
 // Number.MIN_VALUE ->  5.00E-324
  
@@ -25,7 +30,7 @@ const hundreds = 1000000;
 // console.log(Math.round(4.7));
 // 5 -> normal humlog jo round off krte hai woi
 // console.log(Math.ceil(4.1));
-// 5 -> agr 4 se thoda bhi zyada hua to 5 krdo
+// 5 -> agr 4 se thoda bhi zyada hua to 5 krdega
 // console.log(Math.floor(4.8));
 // 4 ->ye ceil ke opposite hai
 // console.log(Math.min(4,8,9,3));
@@ -33,13 +38,13 @@ const hundreds = 1000000;
 // console.log(Math.max(4,8,9,3));
 // for maximum value 9
 // console.log(Math.random());
-// iski value hmesha 0 aur 1 ke bich mai ex:- 7765808915828663
+// 0.38059823444838226 - iski value hmesha 0 aur 1 ke bich mai
 // console.log((Math.random()*10) + 1);
-// 5.904243212518605 is type kibvalue aaegi
-const min = 10
+// console.log(Math.floor((Math.random()*10) + 1));
+// 9 - ye floor se roundoff hoke aaegi value
+ const min = 10
 const max = 20
-
-// console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
  
 
 
