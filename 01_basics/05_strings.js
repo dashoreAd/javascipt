c25aa63f568d4814efc9946de253a5cc8d49bb21
dashoreAd <83025362+dashoreAd@users.noreply.github.com>
2024-04-03 02:11:33 +0000

    edit

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -29,16 +29,18 @@ const anotherString = gameName.slice(1, 4)
 
 const newStringOne = "   hitesh    "
 //  console.log(newStringOne.length);
+// console.log(newStringOne.trim());
 //  total character dega 13
 //  console.log(newStringOne.trim().length);
 //  aage piche ke spaces ko cut krke dega 6
 
 
 const url = "https://hitesh.com/hitesh%20choudhary"
-
+// url mai jo spaces hote hai unhe browser %20 se replace krdeta hai, ab %20 ko ko hume replace krna hai '-' se to hum use krte hai 'replace' method.
 // console.log(url.replace('%20', '-'))
 //  https://hitesh.com/hitesh-choudhary
 // console.log(url.includes('sundar'))
-//  false
+//  false - 'include' method hume search krke btata hai ki code mai ye word(sundar) hai ki ni, hoga to true ni hoga to false.
 // console.log(gameName.split('-'));
 //  [ 'Aditya', 'Dashore' ]
+// split se hum array type mai split kr skte h
