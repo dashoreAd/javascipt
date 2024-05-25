c9923c8fd375c841a1611cf1a7f03b18e8545d76
dashoreAd <83025362+dashoreAd@users.noreply.github.com>
2024-05-25 01:02:58 +0000

    edited

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,6 +1,6 @@
 "use strict"; //treat all Js code as newer version
 
-// alert( 3 + 3 )we are using nodejs, not browser
+// alert( 3 + 3 )we are using nodejs, not browser, kuki ye abhi node mai ni hai browser mai hi hai, to isko browser mai hi run kr skte h.
 
 let name = "Aditya"
 let age = 24;
