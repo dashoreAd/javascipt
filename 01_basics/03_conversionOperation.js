1176b0472d60428f86460d43c3e2e387c6205e4a
dashoreAd <83025362+dashoreAd@users.noreply.github.com>
2024-05-25 01:22:19 +0000

    edited

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -53,7 +53,9 @@ let str3 = str1 + str2
 // console.log( (3+4) * 5 % 3)
 
 // console.log(+true);
+// 1
 // console.log(+"");
+// 0
 
 let num1, num2, num3 
 num1 = num2 = num3 = 2+2
@@ -61,6 +63,7 @@ num1 = num2 = num3 = 2+2
 let gameCounter = 100
 ++gameCounter;
 // console.log(gameCounter);
+// 101
 
 // link to study
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
