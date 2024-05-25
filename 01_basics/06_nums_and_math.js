bc2b8ac031200622f7fe59bb5877e71fbf3a57f7
dashoreAd <83025362+dashoreAd@users.noreply.github.com>
2024-05-25 04:02:15 +0000

    update

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -1,10 +1,10 @@
 const score = 400
 // console.log(score);
+// yhaa pr assume kragya hai
 const balance = new Number(1000)
 // console.log(balance);
-// [Number: 1000] - specially no. likha type milega
+// [Number: 1000] - specifically no. likha type milega
 // console.log(balance.toString());
-
 // 1000 - isse toString se ye string mai hogya, ab bhot sari propety aai
 // console.log(balance.toString().length);
 // 4 ye length btara h 1000 ki
@@ -13,20 +13,24 @@ const balance = new Number(1000)
 // tofixed se woh decimal ke baad do zero tk value dega like 
 // 1000.00
 const otherNumber = 123.8966
-// console.log(otherNumber.toPrecision(4));
+console.log(otherNumber.toPrecision(4));
 // 124 - ye string return krta h
+console.log(typeof(otherNumber.toPrecision(4)));
+// Ye string hai ab return mai.
 const hundreds = 1000000;
 // console.log(hundreds.toLocaleString());
 // 1,000,000 -> US standard ke hisaab se dega
 // console.log(hundreds.toLocaleString('en-IN'));
 // 10,00,000 -> Indian standard ke hisaab se dega
+
+// console.log(Number.MAX_VALUE)
 // Number.MAX_VALUE -> 1.79E+308.
 // Number.MIN_VALUE ->  5.00E-324
  
 // ************************* MATHS *****************************
 // console.log(Math);
 // console.log(Math.abs(-4));
-// 4 -> abs negative waali value ko positive krdeta hai
+// 4 -> absolute value negative waali value ko positive krdeta hai
 // console.log(Math.round(4.7));
 // 5 -> normal humlog jo round off krte hai woi
 // console.log(Math.ceil(4.1));
