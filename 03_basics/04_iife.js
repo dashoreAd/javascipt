65194ebc3af334b20e6ac296edd5523e5fb14f11
dashoreAd <83025362+dashoreAd@users.noreply.github.com>
2024-05-29 12:14:57 +0530

    Update 04_iife.js
    update

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,12 +1,12 @@
 // Immediately Invoked Function Expressions (IIFE)
 // hum chaahte ki jaise hi humara site load ho waise hi kuch data(database se connect hojae) load ho site pe.
-// global scope ke pollutuion se problem hoti hai kai br. to us global scope ke variable declaration ko hatane ke liye iife ko use kiya.
-
+// global scope ke pollutuion(mtlb bhot saare global scope hote hai ek program mai) se problem hoti hai kai br. to us global scope ke variable declaration ko hatane ke liye iife ko use kiya.
+//iife declaration - ()() - pehle wala declaration, dusra wala function execution ke liye //
 (function chai(){
     // named IIFE -> kuki is iife naam h 'chai'
     console.log(`DB CONNECTED`);
 })();
-//semicolon lagana zaruri hai kuki iife invoke to hogya hai pr usko pta ni h kha rukna hai, isliye semicolon must h.
+//semicolon lagana zaruri hai kuki iife invoke to hogya hai pr usko pta ni h ki kha rukna hai context ko, isliye semicolon must h.
 // iife ko parenthese mai cover krna pdta h lin5 & lin8
 ( (name) => {
     // ye simple iife h kuki iska naam ni h
