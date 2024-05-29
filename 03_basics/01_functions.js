26f7392f98dd37f0abb9c8f0813ccf00aa4db6fc
dashoreAd <83025362+dashoreAd@users.noreply.github.com>
2024-05-29 03:56:43 +0000

    updated

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -7,10 +7,8 @@
 //     console.log("S");
 //     console.log("H");
 // }
-// sayMyName
-// ye function ka reference hai
-// sayMyName()
-// ye function ka execution
+// sayMyName :-  ye function ka reference hai
+// sayMyName() :- ye function ka execution
 
 // function addTwoNumbers(number1, number2){
 // console.log(number1 + number2);
@@ -28,31 +26,41 @@
 
 function loginUserMessage(username = "sam"){
     if(!username){
-        console.log("PLease enter a username");
+        // console.log("PLease enter a username"); 
         return
     }
     return `${username} just logged in`
 }
-
+// (!username) - isse hum check krre hai ki user ne koi value daali hai ki nahi, agr no username hai to woh us loop mai chle jaaega aur return krdega, aur agr username h to woh niche waale rfeturn mai chle jaeega.
+// function loginUserMessage(username = "sam"){ --- sam lika hai mtlb default value sam h agr koi value return ni hogi to  sam aajaega, isse fir username waali line likhne se koi mtlb ni h.
 // console.log(loginUserMessage("hitesh"))
 // console.log(loginUserMessage("hitesh"))
  
+
+// ********** NEXT LECTURE *******
+
+
 function calculateCartPrice( ...num1){
     return num1
 }
 // console.log(calculateCartPrice(200, 400, 500, 2000))
 // [ 200, 400, 500, 2000 ]
-
+// rest opeartor(...num1) bhi bolte hai- ye hum jb use krte hai jb situation aise ho like ki ki user ne ek ke baad ek item cart item mai add krte jara hai, to usko ab kaise variable mai daale to hum isko push se bhi kr skte hai, pr ye rest operator waala better hai.
+function CartPrice(val1,val2, ...num1){
+    return num1
+}
+// console.log(CartPrice(200, 400, 500, 2000)) 
+ 
 const user = {
     username: "hitesh",
     prices: 199
 }
 
 function handleObject(anyobject){
-    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+    // console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
 }
 
-// handleObject(user)
+handleObject(user)
 handleObject({
     username: "sam",
     price: 399
