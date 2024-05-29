fa8abe76069d1821ca00297349bac4163ae6e414
dashoreAd <83025362+dashoreAd@users.noreply.github.com>
2024-05-29 14:30:51 +0530

    Update truthy.js
    updated

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -11,31 +11,34 @@ if (userEmail) {
 // false, 0, -0, BigInt 0n, "", null, undefined, NaN
 
 //truthy values
-// "0", 'false', " ", [], {}, function(){}
+// "0","false", " ", [], {}, function(){}
 
 // if (userEmail.length === 0) {
 //     console.log("Array is empty");
 // }
-
+// Array is empty 
 const emptyObj = {}
 
 if (Object.keys(emptyObj).length === 0) {
     console.log("Object is empty");
 }
+// Object is empty
+// false == 0 --- true
+// false == '' --- true
+// 0 == '' --- true
 
 // Nullish Coalescing Operator (??): null undefined
 
 let val1;
-// val1 = 5 ?? 10
-// val1 = null ?? 10
-// val1 = undefined ?? 15
-val1 = null ?? 10 ?? 20
-
+// val1 = 5 ?? 10 --- 5 
+// val1 = null ?? 10 --- 10
+// val1 = undefined ?? 15 --- 15
+val1 = null ?? 10 ?? 20 --- 10
 
 
 console.log(val1);
 
-// Terniary Operator
+// Ternary Operator
 
 // condition ? true : false
 
