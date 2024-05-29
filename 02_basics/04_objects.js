b3e579e7d44dfa3001e106020b1b274f28476540
dashoreAd <83025362+dashoreAd@users.noreply.github.com>
2024-05-29 03:08:45 +0000

    updated

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -33,7 +33,7 @@ const obj2 = {3: "a", 4: "b"}
 // { obj1: { '1': 'a', '2': 'b' }, obj2: { '3': 'a', '4': 'b' } }
 const obj4 = {5: "a", 6: "b"}
 const obj5 = Object.assign({}, obj1, obj2, obj4)
-// {} -> ye target hai aur iske baad wale source hai
+// {} -> ye target hai aur iske baad wale source hai, mtlb ye target aur source return krta hai, isiloye pehle empty array bhejte hai to usse pta chlta hai ki pehla wala target hai aur dusra wala se sb source hai, output same aaega with{} aur w/o.
 // console.log(obj5);
 // { '1': 'a', '2': 'b', '3': 'a', '4': 'b', '5': 'a', '6': 'b' }
 const obj3 = {...obj1, ...obj2}
@@ -56,23 +56,23 @@ const users = [
     },
 ]
 users[1].email
-console.log(tinderUser);
+// console.log(tinderUser);
 // { id: '123abc', name: 'Sammy', isLoggedIn: false }
 
 // console.log(Object.keys(tinderUser));
-// [ 'id', 'name', 'isLoggedIn' ] -> isse key pair ki keyaaegi
-
+// [ 'id', 'name', 'isLoggedIn' ] -> isse key pair ki key aaegi
+ 
 //  console.log(Object.values(tinderUser));
 //  [ '123abc', 'Sammy', false ] -> isse us key ki value aajaaegi
 
 // console.log(Object.entries(tinderUser));
 // [ [ 'id', '123abc' ], [ 'name', 'Sammy' ], [ 'isLoggedIn', false ] ]
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
 //isme property puchte ha ki is loggedIn hai kya, agr loggedIn hogi-yes
 
 
-
+// DESTRUCTURING
 const course = {
     coursename: "js in hindi",
     price: "999",
@@ -81,10 +81,11 @@ const course = {
 
 // course.courseInstructor
 
-const {courseInstructor: instructor} = course
-
+const {courseInstructor} = course
 // console.log(courseInstructor);
-console.log(instructor);
+const {courseInstructor: instructor} = course
+// console.log(courseInstructor);
+// console.log(instructor);
 
 // {
 //     "name": "hitesh",
