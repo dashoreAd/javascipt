c19762ce6b1ef31f7b36afc52e0f8be134d473c3
dashoreAd <83025362+dashoreAd@users.noreply.github.com>
2024-05-31 03:50:44 +0000

    updated

--- 05_iterations/four.js
+++ 05_iterations/four.js
@@ -1,20 +1,36 @@
+// Object ko loop krane ka ek tarika hai for in loop
+
 const myObject = {
     js: "javascript",
     cpp: "C++",
     rb: "ruby",
     swift: "swift by apple"
-}
-// myObject.map((iteration)=>{
-//     console.log(iteration);
-// })  ye wrong method hai
+} 
+
 // Isko for in loop se bna skte hai 
 // for(const key in myObject)
 // {
 //     console.log(`${key} shortcut is for ${myObject[key]}`);
 // }
-// o/p is cpp shortcut is for C++
+// o/p:- cpp shortcut is for C++
 
 const programming = ["js","ruby","py","java","cpp"]
+
+// for (const key in programming) {
+//   console.log(key);
+// }
+// o/p:- 0
+//       1
+//       2
+//       3
+//       4
+
 for (const key in programming) {
- console.log(programming[key]);
-}
+    console.log(programming[key]);
+   }
+   
+// o/p:- js
+//       ruby
+//       py
+//       java
+//       cpp
