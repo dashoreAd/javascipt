bc2ba20dc887b74bbbf01982c7cfc6547c4651da
Aditya Dashore <83025362+dashoreAd@users.noreply.github.com>
2025-03-04 20:49:42 +0530

    Update 02_dataTypes.js

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,6 +1,6 @@
-"use strict"; //treat all Js code as newer version
+"use strict"; //By this Javascript treat all JS code as a newer version
 
-// alert( 3 + 3 )we are using nodejs, not browser, kuki ye abhi node mai ni hai browser mai hi hai, to isko browser mai hi run kr skte h.
+// alert( 3 + 3 ) We are using nodejs, not browser, kuki ye abhi node mai ni hai browser mai hi hai, to isko browser mai hi run kr skte h.
 
 let name = "Aditya"
 let age = 24;
@@ -11,7 +11,7 @@ let isLoggedIn = false
 // bigInt => number se jyada range chaahiye ho to
 // string => ""
 // boolean => true/false
-// null => standAlone value hai
+// null => standAlone value hai,
 // undefined => abhi value assigned ni hui hai
 // symbol => unique
 // object
